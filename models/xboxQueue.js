b6e3811ad1f0467d859e2709195ba6b36017183c
Sabrina Onishi <sabrina.m.onishi@hotmail.com>
2024-11-03 17:36:25 -0300

    Alteração no models

--- models/xboxQueue.js
+++ models/xboxQueue.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
 
 const xboxQueueSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    platform: { type: String, required: true },
-    game: { type: String, required: true },
-    players: [{ 
-      user: { type: String, required: true },
-      dateTime: { type: Date, default: Date.now },
-      positionFila: { type: Number, required: true },
-      console: { type: String, required: true }
-    }]
+  ID: { type: String, required: true },
+  user: { type: String, required: true },
+  dateTime: { type: Date, default: Date.now },
+  positionFila: { type: Number, required: true },
+  console: { type: String, required: true }
   });
 
   const XboxQueue = mongoose.model('XboxQueue', xboxQueueSchema);
