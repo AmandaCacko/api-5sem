d1958641ef3e15282790c3e478ac813f0ae12a1d
Sabrina Onishi <sabrina.m.onishi@hotmail.com>
2024-11-05 03:36:44 -0300

    Correção do novo get

--- routes/queueRoutes.js
+++ routes/queueRoutes.js
@@ -6,7 +6,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.get('/', authMiddleware, queueController.getQueues);
 router.post('/', authMiddleware, queueController.createQueue);
 router.get('/:console', authMiddleware, queueController.getQueuesByConsole);
-router.get('/:username', queueController.getQueuesByUser);
+router.get('/:username', authMiddleware, queueController.getQueuesByUser);
 router.post('/:id/join', authMiddleware, queueController.joinQueue);
 router.post('/:id/leave', authMiddleware, queueController.leaveQueue);
 
